5c51b151702e7b74f5c25904fb9de5e34ddf8c39
Subham Das <subham.das@trigyn.com>
2023-06-04 00:40:07 +0530

    returned all users data and their post count

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,59 @@
 const User = require("../schema/user.schema");
+const Post = require("../schema/post.schema")
 
 module.exports.getUsersWithPostCount = async (req, res) => {
   try {
-    //TODO: Implement this API
+    const page = parseInt(req.query.page) || 1; 
+    const limit = parseInt(req.query.limit) || 10; 
 
-    res.status(200).json({});
+    const startIndex = (page - 1) * limit; // Calculate the starting index of the users for the current page
+
+    const users = await User.find() // Find all users
+      .skip(startIndex) // Skip the users before the starting index
+      .limit(limit) // Limit the number of users per page
+      .exec();
+
+    const totalCount = await User.countDocuments(); // Get the total count of users
+
+    const usersWithPostCount = await Promise.all(
+      users.map(async (user) => {
+        const postCount = await Post.countDocuments({ userId: user._id }); // Get the post count for each user
+        return {
+          _id: user._id,
+          name: user.name,
+          posts: postCount,
+        };
+      })
+    );
+
+    const totalPages = Math.ceil(totalCount / limit); // Calculate the total number of pages
+
+    const hasNextPage = page < totalPages; // Determine if there is a next page
+    const nextPage = hasNextPage ? page + 1 : null; // Calculate the next page number if it exists
+
+    const response = {
+      data: {
+        users: usersWithPostCount,
+        pagination: {
+          totalDocs: totalCount,
+          limit : limit,
+          page : page,
+          totalPages,
+          pagingCounter: startIndex + 1,
+          hasPrevPage: page > 1,
+          hasNextPage,
+          prevPage: page > 1 ? page - 1 : null,
+          nextPage,
+        },
+      },
+    };
+    console.log("response : ", response );
+
+    res.status(200).json(response);
   } catch (error) {
-    res.send({ error: error.message });
-  }
+        res.send({ error: error.message });
+      }
 };
+
+
+
